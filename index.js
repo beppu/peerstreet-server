587e8c5842cca3e9e337c9eabdb1aec2d83138be
John BEPPU <john.beppu@gmail.com>
2018-12-23 01:21:21 -0800

    Implement REST endpoint on /

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 const dotenv     = require('dotenv').config()
 const Koa        = require('koa')
 const bodyParser = require('koa-bodyparser')
+const Router     = require('koa-router')
 
 const db         = require('./lib/db')
+
+
+const port   = process.env.PORT || 3000
+const koa    = new Koa()
+const router = new Router()
+
+router.get('/', async ctx => {
+  const zip = ctx.request.query.zip
+  if (zip) {
+    const msa = await db.msaByZip(zip)
+    if (msa) {
+      ctx.body = {
+        zip,
+        cbsa:            msa.cbsa,
+        name:            msa.name,
+        popestimate2014: msa.popestimate2014,
+        popestimate2015: msa.popestimate2015
+      }
+    } else {
+      ctx.body = {
+        success: false,
+        zip,
+        message: 'MSA for zip code not found.'
+      }
+    }
+  } else {
+    ctx.body = {
+      success: false,
+      message: 'No zip code provided.'
+    }
+  }
+})
+
+koa
+  .use(bodyParser())
+  .use(router.routes())
+  .listen(port, () => {
+    console.log(`> Ready on https://localhost:${port}`)
+  })
