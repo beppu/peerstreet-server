614fe2ea6874e643e1b7cd8655c5833287fec3da
John BEPPU <john.beppu@gmail.com>
2018-12-23 21:13:38 -0800

    Include HTTParty and provide a way to override base_uri

--- peerstreet-client/lib/peerstreet/client.rb
+++ peerstreet-client/lib/peerstreet/client.rb
@@ -3,11 +3,12 @@ require 'json'
 require 'httparty'
 
 class PeerStreet::Client
+  include HTTParty
   base_uri 'https://peerstreet.bavl.org/'
 
-  def initialize options
-    if options.base_uri
-      self.base_uri options.base_uri
+  def initialize(options)
+    if options[:base_uri]
+      self.class.base_uri options[:base_uri]
     end
   end
 
