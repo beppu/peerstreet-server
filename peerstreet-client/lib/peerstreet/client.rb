67f5d20496b389f3e6b5154d88cf6dc2cd898dd9
John BEPPU <john.beppu@gmail.com>
2018-12-23 22:24:56 -0800

    Let the instance have its own base_uri
    I'm not a fan of letting base_uri be defined at the class level.
    What if I want to interact with multiple deployments of the same API?

--- peerstreet-client/lib/peerstreet/client.rb
+++ peerstreet-client/lib/peerstreet/client.rb
@@ -3,17 +3,25 @@ require 'json'
 require 'httparty'
 require_relative 'version'
 
+DEFAULT_BASE_URI = 'https://peerstreet.bavl.org/'
+
 class PeerStreet::Client
   include HTTParty
-  base_uri 'https://peerstreet.bavl.org/'
+  #base_uri 'https://peerstreet.bavl.org/'
 
-  def initialize(options={})
+  attr_accessor :base_uri
+
+  def initialize(options={ :base_uri => DEFAULT_BASE_URI })
     if options[:base_uri]
-      self.class.base_uri options[:base_uri]
+      self.base_uri = options[:base_uri]
     end
   end
 
+  def url_for(route)
+    @base_uri + route
+  end
+
   def msa zip
-    self.class.get('/', { query: { zip: zip } })
+    self.class.get(url_for('/'), { query: { zip: zip } })
   end
 end
